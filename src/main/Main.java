d3c6674958da15516a81a0ede48cd7a5b82f311b
Toth Gabor <tozogabee@gmail.com>
2017-10-19 01:28:44 +0200

    HomeWork3, 10 mins

---
+++ src/main/Main.java
@@ -0,0 +1,47 @@
+package main;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		if(args.length==0)
+		{
+			System.out.println("No any parameters are used");
+		}
+		else
+		{
+			int i=1;
+			for(String tmp : args)
+			{
+			
+				if(isPrime(i))
+				{
+					System.out.println("Hello "+tmp+"!!");
+				}
+				else
+				{
+					System.out.println("Hello "+tmp+"!");
+				}
+				System.out.println(i);
+				++i;
+			}
+		}
+	}
+	
+	public static boolean isPrime(int number)
+	{	
+		for(int i=2;i<number;++i)
+		{
+			if(number/i==0)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
